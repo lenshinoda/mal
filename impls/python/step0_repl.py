2dce192c91ff6681374b85840bc2ed25dc3a5db3
lenshinoda <lebps1996@gmail.com>
2021-04-23 00:39:35 -0500

    step0

--- impls/python/step0_repl.py
+++ impls/python/step0_repl.py
@@ -1,7 +1,30 @@
+import sys
+
+
+def READ(x):
+    return x
+
+
+def EVAL(x):
+    return x
+
+
+def PRINT(x):
+    return x
+
+
+def prep(x):
+    stdin = READ(x)
+    result = EVAL(stdin)
+    printed = PRINT(result)
+    return printed
 
 
 if __name__ == "__main__":
     while True:
-        x = input("user>")
-        e = x
-        print(x)
+        try:
+            user_input = input("user>")
+            x = prep(user_input)
+            print(x)
+        except EOFError:
+            sys.exit()
